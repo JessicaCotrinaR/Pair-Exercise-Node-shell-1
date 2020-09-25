11899e18968018f92d05930b2199ff57741e3f7d
Jessica <jessicacotrina@gmail.com>
2020-09-25 14:23:07 -0400

    The process Global

--- bash.js
+++ bash.js
@@ -1,1 +1,12 @@
-//
+//Ouput a prompt
+process.stdout.write('prompt > ');
+//The stdin 'data' event fires after types a line
+process.stdin.on('data', (data) =>{
+	const cmd = data.toString().trim(); // remove the newline
+	if (cmd === 'pwd'){
+		const pwd = process.cwd();
+		process.stdout.write(pwd);
+	}
+	process.stdout.write('You typed: ' + cmd);
+	process.stdout.write('\nprompt > ');
+});
